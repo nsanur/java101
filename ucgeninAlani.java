100dfce4ad8dffa9bb3ebcc407660cdf1a74e3b0
N <103697226+nsanur@users.noreply.github.com>
2023-05-02 13:08:55 +0300

    Dik Üçgende Hipotenüs Bulan Program
    Formül
    
    Üç𝑔𝑒𝑛𝑖𝑛 ç𝑒𝑣𝑟𝑒𝑠𝑖 = 2𝑢
    
    𝑢 = (a+b+c) / 2
    
    Alan * Alan = 𝑢 * (𝑢 − 𝑎)* (𝑢 − 𝑏) * (𝑢 − 𝑐)

---
+++ ucgeninAlani.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class ucgeninAlani {
+    public static void main(String[] args){
+
+        int a, b;
+        double c, u, alan, cevre;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("a kenarini giriniz: ");
+        a = input.nextInt();
+
+        System.out.print("b kenarini giriniz: ");
+        b = input.nextInt();
+
+        c = Math.sqrt(a*a + b*b);
+        System.out.println("Hipotenus: " + c);
+
+        u = (a+b+c)/2;
+        cevre = 2*u;
+        alan = Math.sqrt(u * (u-a) * (u-b) * (u-c));
+        System.out.println("Ucgenin Cevresi: " + cevre);
+        System.out.println("Ucgenin Alani: " + alan);
+
+    
+    }
+
+}
